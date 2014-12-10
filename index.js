0976b6e769cabcbae768bda82f2421a39482ecfe
David Dao <contact.daviddao@gmail.com>
2014-12-10 20:41:06 +0100

    added event system to tree

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 if (typeof tnt === "undefined") {
     module.exports = tnt = {}
 }
+var eventsystem = require("biojs-events");
+eventsystem.mixin(tnt);
 tnt.utils = require("tnt.utils");
 tnt.tooltip = require("tnt.tooltip");
 tnt.tree = require("./src/index.js");
