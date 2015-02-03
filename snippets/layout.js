c27238cfd650191497d27db957dc2df961a107f1
Miguel Pignatelli <emepyc@gmail.com>
2015-02-03 13:52:38 +0000

    moved selectAncestor to tnt.tooltip

--- snippets/layout.js
+++ snippets/layout.js
@@ -1,22 +1,3 @@
-d3.selection.prototype.selectAncestor = function(type) {
-
-    type = type.toLowerCase();
-
-    var selfNode = this.node();
-    if (selfNode.parentNode === null) {
-	console.log("No more parents");
-	return undefined;
-    }
-
-    var tagName = selfNode.parentNode.tagName;
-
-    if ((tagName !== undefined) && (tagName.toLowerCase() === type)) {
-	return d3.select(selfNode.parentNode);
-    } else {
-	return d3.select(selfNode.parentNode).selectAncestor(type);
-    }
-};
-
 var tnt = require("tnt.tree");
 
 var theme = function() {
