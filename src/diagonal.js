100d913a9d6296cc8aa919d9f3c0d55c27f5d73b
Miguel Pignatelli <emepyc@gmail.com>
2016-04-25 17:18:23 +0100

    fixed problem in chrome (introduced in v50) not showing some branches in vertical trees

--- src/diagonal.js
+++ src/diagonal.js
@@ -3,78 +3,79 @@ var tree = {};
 
 tree.diagonal = function () {
     var d = function (diagonalPath) {
-	var source = diagonalPath.source;
+        var source = diagonalPath.source;
         var target = diagonalPath.target;
         var midpointX = (source.x + target.x) / 2;
         var midpointY = (source.y + target.y) / 2;
         var pathData = [source, {x: target.x, y: source.y}, target];
-	pathData = pathData.map(d.projection());
-	return d.path()(pathData, radial_calc.call(this,pathData))
+        pathData = pathData.map(d.projection());
+        return d.path()(pathData, radial_calc.call(this,pathData));
     };
 
     var api = apijs (d)
-	.getset ('projection')
-	.getset ('path')
-    
+    	.getset ('projection')
+    	.getset ('path');
+
     var coordinateToAngle = function (coord, radius) {
       	var wholeAngle = 2 * Math.PI,
-        quarterAngle = wholeAngle / 4
-	
+        quarterAngle = wholeAngle / 4;
+
       	var coordQuad = coord[0] >= 0 ? (coord[1] >= 0 ? 1 : 2) : (coord[1] >= 0 ? 4 : 3),
-        coordBaseAngle = Math.abs(Math.asin(coord[1] / radius))
-	
+        coordBaseAngle = Math.abs(Math.asin(coord[1] / radius));
+
       	// Since this is just based on the angle of the right triangle formed
-      	// by the coordinate and the origin, each quad will have different 
+      	// by the coordinate and the origin, each quad will have different
       	// offsets
       	var coordAngle;
       	switch (coordQuad) {
       	case 1:
-      	    coordAngle = quarterAngle - coordBaseAngle
-      	    break
+      	    coordAngle = quarterAngle - coordBaseAngle;
+      	    break;
       	case 2:
-      	    coordAngle = quarterAngle + coordBaseAngle
-      	    break
+      	    coordAngle = quarterAngle + coordBaseAngle;
+      	    break;
       	case 3:
-      	    coordAngle = 2*quarterAngle + quarterAngle - coordBaseAngle
-      	    break
+      	    coordAngle = 2*quarterAngle + quarterAngle - coordBaseAngle;
+      	    break;
       	case 4:
-      	    coordAngle = 3*quarterAngle + coordBaseAngle
+      	    coordAngle = 3*quarterAngle + coordBaseAngle;
       	}
-      	return coordAngle
+      	return coordAngle;
     };
 
     var radial_calc = function (pathData) {
-	var src = pathData[0];
-	var mid = pathData[1];
-	var dst = pathData[2];
-	var radius = Math.sqrt(src[0]*src[0] + src[1]*src[1]);
-	var srcAngle = coordinateToAngle(src, radius);
-	var midAngle = coordinateToAngle(mid, radius);
-	var clockwise = Math.abs(midAngle - srcAngle) > Math.PI ? midAngle <= srcAngle : midAngle > srcAngle;
-	return {
-	    radius   : radius,
-	    clockwise : clockwise
-	};
+        var src = pathData[0];
+        var mid = pathData[1];
+        var dst = pathData[2];
+        var radius = Math.sqrt(src[0]*src[0] + src[1]*src[1]);
+        var srcAngle = coordinateToAngle(src, radius);
+        var midAngle = coordinateToAngle(mid, radius);
+        var clockwise = Math.abs(midAngle - srcAngle) > Math.PI ? midAngle <= srcAngle : midAngle > srcAngle;
+        return {
+            radius   : radius,
+            clockwise : clockwise
+        };
     };
 
     return d;
 };
 
 // vertical diagonal for rect branches
-tree.diagonal.vertical = function () {
+tree.diagonal.vertical = function (useArc) {
     var path = function(pathData, obj) {
-	var src = pathData[0];
-	var mid = pathData[1];
-	var dst = pathData[2];
-	var radius = 200000; // Number long enough
+        var src = pathData[0];
+        var mid = pathData[1];
+        var dst = pathData[2];
+        var radius = (mid[1] - src[1]) * 2000;
+        console.log(radius);
 
-	return "M" + src + " A" + [radius,radius] + " 0 0,0 " + mid + "M" + mid + "L" + dst; 
-	
+        return "M" + src + " A" + [radius,radius] + " 0 0,0 " + mid + "M" + mid + "L" + dst;
+        // return "M" + src + " L" + mid + " L" + dst;
     };
 
-    var projection = function(d) { 
-	return [d.y, d.x];
-    }
+    var projection = function(d) {
+        return [d.y, d.x];
+    };
 
     return tree.diagonal()
       	.path(path)
@@ -83,18 +84,17 @@ tree.diagonal.vertical = function () {
 
 tree.diagonal.radial = function () {
     var path = function(pathData, obj) {
-      	var src = pathData[0];
-      	var mid = pathData[1];
-      	var dst = pathData[2];
-	var radius = obj.radius;
-	var clockwise = obj.clockwise;
+        var src = pathData[0];
+        var mid = pathData[1];
+        var dst = pathData[2];
+        var radius = obj.radius;
+        var clockwise = obj.clockwise;
 
-	if (clockwise) {
-	    return "M" + src + " A" + [radius,radius] + " 0 0,0 " + mid + "M" + mid + "L" + dst; 
-	} else {
-	    return "M" + mid + " A" + [radius,radius] + " 0 0,0 " + src + "M" + mid + "L" + dst;
-	}
-
+        if (clockwise) {
+            return "M" + src + " A" + [radius,radius] + " 0 0,0 " + mid + "M" + mid + "L" + dst;
+        } else {
+            return "M" + mid + " A" + [radius,radius] + " 0 0,0 " + src + "M" + mid + "L" + dst;
+        }
     };
 
     var projection = function(d) {
@@ -104,7 +104,7 @@ tree.diagonal.radial = function () {
 
     return tree.diagonal()
       	.path(path)
-      	.projection(projection)
+      	.projection(projection);
 };
 
 module.exports = exports = tree.diagonal;
